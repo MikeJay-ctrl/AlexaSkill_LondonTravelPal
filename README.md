d272a9f5686e8df95bbe1e939570d19be1ce5a15
Michael James <michaeljames@Admin.local>
2017-07-15 11:06:21 +0100

    updates to make speech output more concise

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Alexa London Travel Pal 
-Alexa Skill for givin gthe status of London (Underground and Overground) Lines 
+Alexa Skill for giving the status of London (Underground and Overground) Lines 
--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var 	fs 			= require('fs');
 var properties
 var APP_ID 		
 var BASE_URL 	
-var STOP_MESSAGE
+var STOP_MSGE
+var INVALID_LINE_ERR_MSG
+var ALL_LINES_ERR_MSG
+var NO_LINE_ERR_MSG
 var HEADERS = {}
 
 //Remove prior to deployment
@@ -16,16 +19,21 @@ fs.readFile('./cfgs/config.json', 'utf8', function(err,data){
 	if (err) throw err;
 	properties = JSON.parse(data);
 
-	APP_ID 				= properties.AMAZON_OBJECT.APP_ID;
-	BASE_URL 			= properties.API_OBJECT.BASE_URL;
-	STOP_MESSAGE 		= properties.API_OBJECT.STOP_MESSAGE;
-	HEADERS.app_key 	= properties.API_OBJECT.app_key;
-	HEADERS.app_id 		= properties.API_OBJECT.app_id;
+	APP_ID 					= properties.AMAZON_OBJECT.APP_ID;
+	BASE_URL 				= properties.API_OBJECT.BASE_URL;
+
+	HEADERS.app_key 		= properties.API_OBJECT.app_key;
+	HEADERS.app_id 			= properties.API_OBJECT.app_id;
+
+	STOP_MSG 	 			= properties.CONSTANTS.STOP_MSG;
+	NO_LINE_ERR_MSG 	 	= properties.CONSTANTS.NO_LINE_ERR_MSG;
+	INVALID_LINE_ERR_MSG	= properties.CONSTANTS.INVALID_LINE_ERR_MSG;
+	ALL_LINES_ERR_MSG		= properties.CONSTANTS.ALL_LINES_ERR_MSG
 	//console.log(HEADERS)
 	//console.log(BASE_URL)
 	//console.log(APP_ID)
 	//console.log(STOP_MESSAGE)
-	//getStatusUpdates('Victoria').then(console.log)
+	getStatusUpdates().then(console.log).catch(console.log)
 });
 
 
@@ -39,33 +47,43 @@ function getStatusUpdates(line){
 			var groups = {}
 			var data = JSON.parse(body);
 
-			(data).map(line => {
+			//Group line data by status
+			(data).forEach(line => {
 				var service = line.lineStatuses[0].statusSeverityDescription
 				groups = (groups[service] === undefined ? Object.assign(groups, {[service]: []}) : groups)
 				groups[service].push(line.name)
 
 			});
 			
+			//Order grouped object by number of lines with each status: ASC
+			var currentServiceTypes = Object.keys(groups).sort((a, b) => {return groups[a].length - groups[b].length})
+
 
 			var sentence = []
-			if(line === undefined && Object.keys(groups).length === 1){
-				return "You will find " + Object.keys(groups)[0] + " on all london underground lines."
-			}
-			Object.keys(groups).map( (service, idx) => {
+			var service
+			//classic for loop needed because of break statement
+			for(var s_idx = 0; s_idx < currentServiceTypes.length; s_idx++){
+				var service = currentServiceTypes[s_idx]
+				var numLinesWithService = groups[service].length
 
-				var lines = groups[service].length
-				groups[service].map(line => {
+				//allows for shrtened sentences, not explicitly listing each line
+				if(line===undefined && s_idx === currentServiceTypes.length-1 && numLinesWithService > 1){
+					sentence.push("You will find " + service + " on all" + (currentServiceTypes.length === 1 ? "": " other") + " London underground lines.");
+					break;
+				}
+
+				groups[service].forEach((line, idx) => {
 						sentence.push(idx === 0 ? "The " + line : line)
 				})
 		
-				if(lines > 0){
-					sentence.push((lines > 1 ? " Lines have " : " Line has ") + service + ".")
+				if(numLinesWithService > 0){
+					sentence[sentence.length-1] = sentence[sentence.length-1] + ((numLinesWithService > 1 ? " Lines have " : " Line has ") + service + ".")
 				}
-				
-			})
-			return sentence.join(line === undefined? ", " : "").replace(new RegExp('&', 'g'), 'and');
+
+			}
+			return sentence.join(" ").replace(new RegExp('&', 'g'), 'and');
 		})
-		.catch(()=> { return "error retriving journey status" });
+		.catch(()=> { return line === undefined ? ALL_LINES_ERR_MSG : line.length > 0 ? line + INVALID_LINE_ERR_MSG + NO_LINE_ERR_MSG : NO_LINE_ERR_MSG });
 }
 
 
